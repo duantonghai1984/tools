9b8e55b331d6ebdfbf674ff0a047011c1354e77a
tony <tony.duan@zhongan.com>
2017-05-25 18:03:47 +0800

    去掉无用的

--- js/dva-test/src/router.js
+++ js/dva-test/src/router.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
 
-import LoginPage from "./routes/LoginPage.js";
+import SearchPage from "./routes/SearchPage.js";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
-      <Route path="/LoginPage" component={LoginPage} />
+      <Route path="/" component={SearchPage} />
     </Router>
   );
 }
